2cbec754c05c544ad337d44d2169692a25537c9c
Takahiro Hodumi <fatrow@googlemail.com>
2010-04-29 22:45:16 +0900

    fix README.textile

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject eyewrap "1.0.0-SNAPSHOT"
+(defproject eyewrap "0.5.0"
   :description "Execution observe tool."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+		     [leiningen/lein-swank "1.1.0"]])
