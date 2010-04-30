ead1951972f85330fa7ed99a3ed6567630930c2b
Takahiro Hodumi <fatrow@googlemail.com>
2010-05-01 02:35:04 +0900

    project.clj 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eyewrap "0.5.1"
+(defproject eyewrap "0.5.2"
   :description "Execution observe tool."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
